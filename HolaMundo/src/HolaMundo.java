e447114be4eb67fad36c8ecb7608021998d33bfb
Cristian Fernández <cftojo86@gmail.com>
2019-10-01 18:42:56 +0200

    añadido piramide

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo " + (9/2));
 		System.out.println();
 		
 		new Coche();
@@ -14,6 +14,22 @@ public class HolaMundo {
 		System.out.println();
 		System.out.println("FIN DE EJECUCIÓN");
 
+		System.out.println("\nPIRÁMIDE ASTERISCOS:");
+		
+		
+		int altura = 9 / 2;
+		for (int i = 0; i < 5; i++) {
+			System.out.println();
+			
+			for (int j = 0; j < 9; j++) {
+				if (j > (altura + i) && j < (altura - i)) {
+					System.out.print(" ");
+				} else {
+					System.out.print("* ");
+				}
+				altura++;
+			}
+		}
 	}
 
 }
